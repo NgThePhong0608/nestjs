575c096826028d8b26011172abd2eca03a0c7e03
phongnguyenthe <phong.nguyenthe@amela.vn>
2025-03-11 11:19:32 +0700

    Controller and Route handle

---
+++ src/users/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Delete, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all users';
+  }
+
+  @Delete('/by-id')
+  findById(): string {
+    return 'This action will delete a user by id';
+  }
+}
